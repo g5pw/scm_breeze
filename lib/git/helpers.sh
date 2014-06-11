783dc0bef2c9910f9ad7651cfd6a1208306b67d5
Aljaž "g5pw" Srebrnič <a2piratesoft@gmail.com>
2014-06-11 14:45:12 +0200

    Move useful function to common file
    Also, move the GIT_BINARY setting to git helpers. It could also live in the
    common file, but it looks more sane to set it there, I think.

--- lib/git/helpers.sh
+++ lib/git/helpers.sh
@@ -1,11 +1,4 @@
-function find_in_cwd_or_parent() {
-  local slashes=${PWD//[^\/]/}; local directory=$PWD;
-  for (( n=${#slashes}; n>0; --n )); do
-    test -e "$directory/$1" && echo "$directory/$1" && return 0
-    directory="$directory/.."
-  done
-  return 1
-}
+export GIT_BINARY=$(find_binary git)
 
 function fail_if_not_git_repo() {
   if ! find_in_cwd_or_parent ".git" > /dev/null; then
--- lib/scm_breeze.sh
+++ lib/scm_breeze.sh
@@ -23,7 +23,14 @@ find_binary(){
   fi
 }
 
-export GIT_BINARY=$(find_binary git)
+function find_in_cwd_or_parent() {
+  local slashes=${PWD//[^\/]/}; local directory=$PWD;
+  for (( n=${#slashes}; n>0; --n )); do
+    test -e "$directory/$1" && echo "$directory/$1" && return 0
+    directory="$directory/.."
+  done
+  return 1
+}
 
 # Updates SCM Breeze from GitHub.
 update_scm_breeze() {
